5b5400badfe9f65ef9cce5d1ced54dd9e4a29cd9
Aaron Noe <aarminoe@gmail.com>
2022-05-11 18:30:19 -0500

    commit tree df041a8e9ac9213b2ca5969e7b2699ecc2d98a54

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    let firstLetter = string.slice(0,1).toUpperCase()
+    let restOfString = string.slice(1)
+    return firstLetter + restOfString
+  }
+
+  static sanitize(string) {
+    if (string.length === 1 && string.match(/[a-z]/i)) {
+      return string
+    }
+    return string.replace(/[^a-z0-9' -]/gi, '')    
+  }
+
+  static titleize(string) {
+    const badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let stringArray = string.split(' ')
+    let wordArray = []
+    stringArray.forEach((s) => {
+      badWords.forEach(() => {
+        if (badWords.indexOf(s) === -1 || s === stringArray[0]) {
+          let firstLetter = s.slice(0,1).toUpperCase()
+          let restOfString = s.slice(1)
+          let titleWord = firstLetter + restOfString
+          wordArray.push(titleWord)
+        }
+        else {
+          wordArray.push(s)
+        }
+      })
+    })
+    let newUniqueString = [...new Set(wordArray)]
+    let finalString  = newUniqueString.join(' ')
+    return finalString
+  }
+
+  static titleize(string) {
+    const badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let stringArray = string.split(' ')
+    let wordArray = []
+    stringArray.forEach((s) => {
+      if (badWords.indexOf(s) === -1 || s === stringArray[0]) {
+        let firstLetter = s.slice(0,1).toUpperCase()
+        let restOfString = s.slice(1)
+        let titleWord = firstLetter + restOfString
+        wordArray.push(titleWord)
+      }
+      else {
+        wordArray.push(s)
+      }
+    })
+    let finalString  = wordArray.join(' ')
+    return finalString
+  }
 }
+
